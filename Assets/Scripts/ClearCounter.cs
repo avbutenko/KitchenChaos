7a6ad01e70bf63564ffc53b5f5e895b0f18eec19
avbutenko <butenko.92@mail.ru>
2023-04-29 00:23:57 +0300

    lecture 13 is done

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCounter : MonoBehaviour
+{
+    public void Intercat()
+    {
+
+
+    }
+}
+
+
+
